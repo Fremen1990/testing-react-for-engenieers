8e654d2bca9f4bab7ab2351ee37eae2373313bf1
Tomasz Stanisz <fremen1990@gmail.com>
2023-03-04 16:48:13 +0100

    :zap: Section 4: Test react hooks

--- src/__tests__/unit/hooks/case-6-useFilters.test.tsx
+++ src/__tests__/unit/hooks/case-6-useFilters.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 
-import { useFilters } from '../../../hooks/useFilters'
-import { FiltersContext } from '../../../context/filters'
+import {useFilters} from '../../../hooks/useFilters'
+import {FiltersContext} from '../../../context/filters'
 
 describe('The useFilters hook', () => {
-  it('❌ returns the current value of the filters context', () => {})
+
+    const Panel = () => {
+        const {toggleShowingFilters} = useFilters()
+        return <button onClick={toggleShowingFilters}>Toggle</button>
+    }
+
+    it('❌ returns the current value of the filters context', () => {
+        const toggleShowingFilters = jest.fn()
+        const {getByText, debug} = render(<FiltersContext.Provider value={{toggleShowingFilters} as any}>
+            <Panel/>
+        </FiltersContext.Provider>)
+
+        debug()
+
+        fireEvent.click(getByText('Toggle'))
+        expect(toggleShowingFilters).toHaveBeenCalled()
+    })
 })
